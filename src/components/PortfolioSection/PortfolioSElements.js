aaaea5775809393f413fc755f5cca79e1aa43f40
William Marchesi <wmarchesi123@gmail.com>
2021-10-04 13:43:50 -0400

    Portfolio Elements Outline Complete

--- src/components/PortfolioSection/PortfolioSElements.js
+++ src/components/PortfolioSection/PortfolioSElements.js
@@ -25,3 +25,45 @@ export const PortfolioH1 = styled.h1`
     width: 100%;
     text-align: center;
 `
+
+export const PortfolioBody = styled.div`
+    width: auto;
+    margin: 0 5%;
+    height: auto;
+    background: transparent;
+    display: block;
+    justify-content: space-between;
+`
+
+export const PortfolioElement = styled.div`
+    width: 100%;
+    //height: 100%;
+    border-radius: 2rem;
+    padding: 1rem 2rem;
+    margin: 2rem 0;
+    padding-bottom: 2rem;
+    display: flex;
+`
+
+export const PortfolioElementBody = styled.div`
+    width: 60%;
+    background: #b6f0ff;
+    border-radius: 2rem;
+    padding: 1rem 2rem;
+    margin: 2rem 0;
+    padding-bottom: 2rem;   
+`
+
+export const PortfolioElementImage = styled.img`
+    width: 30%;
+    height: auto;
+    margin: 0 auto;
+    position: relative;
+`
+
+export const PortfolioElementH1 = styled.h1`
+    color: #282948;
+    font-size: 30px;
+    text-align: center;
+    font-weight: 400;
+`
--- src/components/PortfolioSection/index.js
+++ src/components/PortfolioSection/index.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import { PortfolioContainer, PortfolioH1 } from './PortfolioSElements'
+import { PortfolioBody, PortfolioContainer, PortfolioElement, PortfolioElementBody, PortfolioElementH1, PortfolioElementImage, PortfolioH1 } from './PortfolioSElements'
 
 const PortfolioSection = () => {
     return (
         <PortfolioContainer>
             <PortfolioH1>Portfolio</PortfolioH1>
+
+            <PortfolioBody>
+                <PortfolioElement>
+                    <PortfolioElementBody>
+                        <PortfolioElementH1>RVU Track (iOS)</PortfolioElementH1>
+                    </PortfolioElementBody>
+
+                    <PortfolioElementImage src="/" />
+                </PortfolioElement>
+
+                <PortfolioElement>
+                    <PortfolioElementImage src="/" />
+
+                    <PortfolioElementBody>
+                        <PortfolioElementH1>Personal Website</PortfolioElementH1>
+                    </PortfolioElementBody>
+                </PortfolioElement>
+
+            </PortfolioBody>
         </PortfolioContainer>
     )
 }
